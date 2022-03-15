58511046670de7e868964fb0520802d16c49a384
Kavindu handapangoda <kavinduhandapangoda@gmail.com>
2022-03-15 14:41:03 +0530

    test cases

--- tests/Feature/googlemapTest.php
+++ tests/Feature/googlemapTest.php
@@ -3,18 +3,20 @@
 namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-class ExampleTest extends TestCase
+class googlemapTest extends TestCase
 {
     /**
-     * A basic test example.
+     * A basic feature test example.
      *
      * @return void
      */
     public function test_example()
     {
-        $response = $this->get('/d');
+        $response = $this->get('/map');
+
         $response->assertStatus(200);
     }
 }
